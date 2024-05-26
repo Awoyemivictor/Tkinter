e9ecfe2b32140d03572990302a671891f0579fb8
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2024-05-26 12:48:07 +0100

    Tkinter

---
+++ distance_in_meters.py
@@ -0,0 +1,47 @@
+# Incorporating Tk
+from tkinter import *
+from tkinter import ttk
+
+# Our calculation function
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        pass
+
+# Setting up the Main Application Window    
+root = Tk()
+root.title("Feet to Meters")
+
+# Creating a Content Frame
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+# Creating the Entry Widget
+feet = StringVar()
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+# Creating the Remaining Widgets
+meters = StringVar()
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+
+# Telling Tk to put the focus on our entry widget so cursor will start in the field    
+feet_entry.focus()
+# If user presses the Return key (Enter on Windows), it should call our calculate function
+root.bind("<Return>", calculate)
+
+# To start the Event Loop for everything to appear oncscreen and allow users to interact with it.
+root.mainloop()
